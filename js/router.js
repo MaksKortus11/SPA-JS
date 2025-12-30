cf1dff25bea30cd089cd6932acfa913cee30c68a
MaksKortus11 <adiseba11@gmail.com>
2025-12-30 08:59:36 +0100

    Update router.js

--- js/router.js
+++ js/router.js
@@ -14,14 +14,14 @@ OnStartUp();
 document.querySelector('#about-link').addEventListener('click', () => {
   let stateObj = { page: 'about' };
   document.title = 'About';
-  history.pushState(stateObj, "about", "?about");
+  history.pushState(stateObj, "about", "index.html?about");
   RenderAboutPage();
 });
 
 document.querySelector('#contact-link').addEventListener('click', () => {
   let stateObj = { page: 'contact' };
   document.title = 'Contact';
-  history.pushState(stateObj, "contact", "?contact");
+  history.pushState(stateObj, "contact", "index.html?contact");
   RenderContactPage();
 });
 
@@ -58,14 +58,14 @@ function RenderContactPage() {
 /* --- Obsługa przycisków wstecz/dalej --- */
 
 function popStateHandler() {
-  let loc = window.location.href.toString().split(window.location.host)[1];
+  const query = window.location.search;
 
-  if (loc === pageUrls.contact) {
-    RenderContactPage();
+  if (query === '?about') {
+    RenderAboutPage();
   }
 
-  if (loc === pageUrls.about) {
-    RenderAboutPage();
+  if (query === '?contact') {
+    RenderContactPage();
   }
 }
 
